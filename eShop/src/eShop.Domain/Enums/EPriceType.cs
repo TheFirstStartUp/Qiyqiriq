bd67b18f40fbf53c4f4ddfd9a28c75ac66dcbb67
Sardor <studentx201@tuit.uz>
2024-08-17 22:34:50 +0500

    Enums

---
+++ eShop/src/eShop.Domain/Enums/EPriceType.cs
@@ -0,0 +1,9 @@
+﻿namespace eShop.Domain.Enums
+{
+    public enum EPriceType
+    {
+        Sale,                 // Chegirma qilingan narx
+        Installment,          // Rasrochka narxi (bo'lib to'lash uchun narx)
+        Price,                // Tovarning original narxi (asosiy narx)
+    }
+}
