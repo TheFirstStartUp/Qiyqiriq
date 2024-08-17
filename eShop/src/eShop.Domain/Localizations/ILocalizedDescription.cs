e7410cbdc2fd38788e4eeda7ffc9facff584d26c
Sardor <studentx201@tuit.uz>
2024-08-17 22:34:34 +0500

    Interfaces for localization

---
+++ eShop/src/eShop.Domain/Localizations/ILocalizedDescription.cs
@@ -0,0 +1,9 @@
+﻿namespace eShop.Domain.Localizations
+{
+    public interface ILocalizedDescription
+    {
+        public string DescriptionUz { get; set; }
+        public string DescriptionRu { get; set; }
+        public string DescriptionEn { get; set; }
+    }
+}
---
+++ eShop/src/eShop.Domain/Localizations/ILocalizedName.cs
@@ -0,0 +1,9 @@
+﻿namespace eShop.Domain.Localizations
+{
+    public interface ILocalizedName
+    {
+        public string NameUz { get; set; }
+        public string NameRu { get; set; }
+        public string NameEn { get; set; }
+    }
+}
