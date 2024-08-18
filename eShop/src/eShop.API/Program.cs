90fe85d9e0aab9b6bdeee0ed89f44f35d7e4c73e
Sardor <studentx201@tuit.uz>
2024-08-18 19:14:32 +0500

    Infrastructure DI

--- eShop/src/eShop.API/Program.cs
+++ eShop/src/eShop.API/Program.cs
@@ -1,10 +1,15 @@
 using eShop.API.Extensions;
+using eShop.Application;
+using eShop.Infrastructure;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddInfrastructureServices(builder.Configuration);
+builder.Services.AddApplicationServices();
 
 var app = builder.Build();
 
---
+++ eShop/src/eShop.Infrastructure/DependencyInjection.cs
@@ -0,0 +1,18 @@
+﻿using eShop.Infrastructure.Context;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace eShop.Infrastructure
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddInfrastructureServices(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddDbContext<eShopDbContext>(options
+                => options.UseNpgsql(configuration.GetConnectionString("PosgresDb")));
+
+            return services;
+        }
+    }
+}
