b072729436e0eab235cc7dd232ed4c10695b8bf6
Sardor <studentx201@tuit.uz>
2024-08-19 23:34:26 +0500

    Test Controller

---
+++ eShop/src/eShop.API/Controllers/TestController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace eShop.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TestController : ControllerBase
+    {
+        [HttpGet]
+        public async Task<IActionResult> TestActionAsync()
+        {
+            return Ok("Qalesiz! Xush kelibsiz, Qiyqiriqqa!");
+        }
+    }
+}
