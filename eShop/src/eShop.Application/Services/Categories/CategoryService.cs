150a524e995f2c2d3458f4920af5c01bcdb3bd02
Sardor <studentx201@tuit.uz>
2024-08-29 23:56:11 +0500

    Category Service implemented

---
+++ eShop/src/eShop.Application/Services/Categories/CategoryService.cs
@@ -0,0 +1,70 @@
+﻿using eShop.Application.DataTransferObjects.Categories;
+using eShop.Domain.Enitities.Products;
+using eShop.Infrastructure.Repositories.Products.Categories;
+using Mapster;
+
+namespace eShop.Application.Services.Categories
+{
+    public class CategoryService : ICategoryService
+    {
+        private readonly ICategoryRepository _categoryRepository;
+
+        public CategoryService(ICategoryRepository categoryRepository) 
+            => _categoryRepository = categoryRepository;
+
+        public async Task<Category> AddCategoryAsync(CategoryCreationDTO categoryCreationDTO)
+        {
+            var category = categoryCreationDTO.Adapt<Category>();
+            category = await _categoryRepository.AddAsync(category);
+
+            return category;
+        }
+
+        public async Task<Category> DeleteCategoryAsync(int id)
+        {
+            var category = await _categoryRepository.FindAsync(x => x.Id.Equals(id));
+
+            if(category == null) 
+            {
+                //validate
+            }
+            
+            category = await _categoryRepository.RemoveAsync(category);
+
+            return category;
+        }
+
+        public async Task<List<Category>> GetAllCategoriesAsync()
+        {
+            return _categoryRepository.GetAll().ToList();
+        }
+
+        public async Task<Category> GetByIdCategoryAsync(int id)
+        {
+            var category = await _categoryRepository.FindAsync(x => x.Id.Equals(id));
+
+            if(category == null) 
+            {
+                // validate
+            }
+
+            return category;
+        }
+
+        public async Task<List<Category>> GetCategiesByParentIdCategoryAsync(int id) 
+            => await _categoryRepository.Filter(x => x.ParentId == id);
+
+        public async Task<Category> UpdateCategoryAsync(int id,CategoryModificationDTO categoryModificationDTO)
+        {
+            var category = await _categoryRepository.FindAsync(x => x.Id.Equals(id));
+
+            if(category == null) 
+            {
+                // validate
+            }
+
+            category = categoryModificationDTO.Adapt(category);
+            return await _categoryRepository.UpdateAsync(category);
+        }
+    }
+}
---
+++ eShop/src/eShop.Application/Services/Categories/ICategoryService.cs
@@ -0,0 +1,15 @@
+﻿using eShop.Application.DataTransferObjects.Categories;
+using eShop.Domain.Enitities.Products;
+
+namespace eShop.Application.Services.Categories
+{
+    public interface ICategoryService
+    {
+        Task<Category> AddCategoryAsync(CategoryCreationDTO categoryCreationDTO);
+        Task<Category> GetByIdCategoryAsync(int id);
+        Task<List<Category>> GetCategiesByParentIdCategoryAsync(int id);
+        Task<List<Category>> GetAllCategoriesAsync();
+        Task<Category> UpdateCategoryAsync(int id,CategoryModificationDTO categoryModificationDTO);
+        Task<Category> DeleteCategoryAsync(int id);
+    }
+}
