497de1882f171c92673a36521bdbbb5071fbf6ee
Sardor <studentx201@tuit.uz>
2024-08-29 23:56:21 +0500

    DTO s

---
+++ eShop/src/eShop.Application/DataTransferObjects/Categories/CategoryCreationDTO.cs
@@ -0,0 +1,11 @@
+﻿namespace eShop.Application.DataTransferObjects.Categories
+{
+    public record CategoryCreationDTO
+    (
+        string NameUz,
+        string NameRu,
+        string NameEn,
+
+        int? ParentId
+    );
+}
